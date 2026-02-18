015891d86f3f733d9ce1a801d627acb4fabc8af1
catalexandre <catellier.alexandre@gmail.com>
2026-02-18 00:07:32 -0500

    Linked UI to LoginServices and SignUpServices for both login and signup page

--- app/src/main/java/com/example/ticketnoob/ui/activities/LoginActivity.java
+++ app/src/main/java/com/example/ticketnoob/ui/activities/LoginActivity.java
@@ -1,14 +1,35 @@
 package com.example.ticketnoob.ui.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ticketnoob.R;
+import com.example.ticketnoob.repository.UserRepository;
+import com.example.ticketnoob.service.LoginService;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText etEmailOrPhone;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    private LoginService loginService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmailOrPhone = findViewById(R.id.etEmailOrPhone);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        UserRepository userRepository = new UserRepository();
+
+        loginService = new LoginService(this, userRepository, etEmailOrPhone, etPassword, btnLogin);
+        loginService.init();
     }
 }
