bc8b242a3c769b5a011692b8d982f5ea67e49979
Pamela Daniel <149453946+bypameladaniel@users.noreply.github.com>
2026-03-31 21:57:07 -0400

    Add some more Login functional tests

--- app/src/androidTest/java/com/example/ticketnoob/service/LoginActivityFunctionalTest.java
+++ app/src/androidTest/java/com/example/ticketnoob/service/LoginActivityFunctionalTest.java
@@ -1,4 +1,4 @@
-package com.example.ticketnoob.ui.activities;
+package com.example.ticketnoob.service;
 
 import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
@@ -6,6 +6,8 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
+import static org.hamcrest.Matchers.not;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
@@ -35,5 +37,42 @@ public class LoginActivityFunctionalTest {
 
         onView(withId(R.id.etEmailOrPhone))
                 .check(matches(hasErrorText("Provide email or phone")));
+    }
+
+    @Test
+    public void login_emptyPassword_showsFieldError() {
+        launchActivity();
+
+        onView(withId(R.id.etEmailOrPhone)).perform(replaceText("john@test.com"));
+        closeSoftKeyboard();
+
+        onView(withId(R.id.btnLogin)).perform(click());
+
+        onView(withId(R.id.etPassword))
+                .check(matches(hasErrorText("Provide password")));
+    }
+
+    @Test
+    public void login_invalidCredentials_buttonEventuallyReturnsToLogin() throws InterruptedException {
+        launchActivity();
+
+        onView(withId(R.id.etEmailOrPhone)).perform(replaceText("notarealuser@test.com"));
+        onView(withId(R.id.etPassword)).perform(replaceText("wrongpass"));
+        closeSoftKeyboard();
+
+        onView(withId(R.id.btnLogin)).perform(click());
+
+        // Immediately after click, button should enter loading state
+        onView(withId(R.id.btnLogin))
+                .check(matches(withText("Logging in...")))
+                .check(matches(not(isEnabled())));
+
+        // Give async Firebase/auth callback time to complete
+        Thread.sleep(3000);
+
+        // After callback, button should return to normal state
+        onView(withId(R.id.btnLogin))
+                .check(matches(withText("Login")))
+                .check(matches(isEnabled()));
     }
 }
